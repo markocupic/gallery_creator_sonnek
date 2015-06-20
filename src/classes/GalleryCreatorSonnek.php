cfe3e86f3a19c13c0b17afdcf12f812be00d7241
Marko Cupic <m.cupic@gmx.ch>
2015-06-20 09:55:00 +0200

    reformat code

--- src/classes/GalleryCreatorSonnek.php
+++ src/classes/GalleryCreatorSonnek.php
@@ -40,80 +40,65 @@ class GalleryCreatorSonnek extends \System
         // Get album model
         $objAlbum = \MCupic\GalleryCreatorAlbumsModel::findAll();
 
-        if ($objAlbum === null)
-        {
+        if ($objAlbum === null) {
             return;
         }
-        while ($objAlbum->next())
-        {
-            if (!$objAlbum->observeAssignedDir)
-            {
+        while ($objAlbum->next()) {
+            if (!$objAlbum->observeAssignedDir) {
                 continue;
             }
 
+            // Store all image path of the album in $arrImgSRC
             $arrImgSRC = [];
+
 
             // Case 1:
             // Clean Datarecords if there is no related file on the server
-
-                $objPictureModel = \MCupic\GalleryCreatorPicturesModel::findByPid($objAlbum->id);
-                if ($objPictureModel !== null)
-                {
-                    while ($objPictureModel->next())
-                    {
-                        if (\Validator::isUuid($objPictureModel->uuid))
-                        {
-                            $objFilesModel = \FilesModel::findByUuid($objPictureModel->uuid);
-                            if ($objFilesModel !== null)
-                            {
-                                $arrImgSRC[] = $objFilesModel->path;
-                                if($objAlbum->deleteOrphanedDatarecords)
-                                {
-                                    if (!is_file(TL_ROOT . '/' . $objFilesModel->path))
-                                    {
-                                        \System::log('DELETE FROM tl_gallery_creator_pictures WHERE id=' . $objPictureModel->id, __METHOD__, TL_GENERAL);
-                                        $objPictureModel->delete();
-                                    }
+            $objPictureModel = \MCupic\GalleryCreatorPicturesModel::findByPid($objAlbum->id);
+            if ($objPictureModel !== null) {
+                while ($objPictureModel->next()) {
+                    if (\Validator::isUuid($objPictureModel->uuid)) {
+                        $objFilesModel = \FilesModel::findByUuid($objPictureModel->uuid);
+                        if ($objFilesModel !== null) {
+                            $arrImgSRC[] = $objFilesModel->path;
+                            if ($objAlbum->deleteOrphanedDatarecords) {
+                                if (!is_file(TL_ROOT . '/' . $objFilesModel->path)) {
+                                    \System::log('DELETE FROM tl_gallery_creator_pictures WHERE id=' . $objPictureModel->id, __METHOD__, TL_GENERAL);
+                                    $objPictureModel->delete();
                                 }
                             }
                         }
                     }
                 }
+            }
 
 
             // Case 2:
             // Check the album directory for some new files
             $arrNewFiles = array();
             $objFolderModel = \FilesModel::findByUuid($objAlbum->assignedDir);
-            if ($objFolderModel === null)
-            {
+            if ($objFolderModel === null) {
                 continue;
             }
-            if ($objFolderModel->type != 'folder')
-            {
+            if ($objFolderModel->type != 'folder') {
                 continue;
             }
 
             // Scan the album directory
             $arrFiles = scan(TL_ROOT . '/' . $objFolderModel->path);
 
-            foreach ($arrFiles as $strPath)
-            {
+            foreach ($arrFiles as $strPath) {
 
-                if (is_file(TL_ROOT . '/' . $objFolderModel->path . '/' . $strPath))
-                {
+                if (is_file(TL_ROOT . '/' . $objFolderModel->path . '/' . $strPath)) {
                     $strFileSRC = $objFolderModel->path . '/' . $strPath;
                     $objFile = new \File($strFileSRC);
-                    if ($objFile->isGdImage)
-                    {
-                        if (in_array($strFileSRC, $arrImgSRC))
-                        {
+                    if ($objFile->isGdImage) {
+                        if (in_array($strFileSRC, $arrImgSRC)) {
                             // Continue, if the image-file is already member ob the album
                             continue;
                         }
 
-                        if (in_array($objFile->extension, explode(',', self::$acceptedFiles)))
-                        {
+                        if (in_array($objFile->extension, explode(',', self::$acceptedFiles))) {
 
                             // clean filename
                             $strNewName = $objFile->dirname . '/' . $objFile->filename . '.' . $objFile->extension;
@@ -122,13 +107,11 @@ class GalleryCreatorSonnek extends \System
                             $strNewName = utf8_romanize($strNewName);
                             $strNewName = str_replace('"', '', $strNewName);
                             $strNewName = str_replace(' ', '_', $strNewName);
-                            if (preg_match('/\.$/', $strNewName))
-                            {
+                            if (preg_match('/\.$/', $strNewName)) {
                                 throw new Exception($GLOBALS['TL_LANG']['ERR']['invalidName']);
                             }
 
-                            if ($objFile->renameTo($strNewName))
-                            {
+                            if ($objFile->renameTo($strNewName)) {
                                 $arrNewFiles[] = $strNewName;
                             }
                         }
@@ -136,8 +119,7 @@ class GalleryCreatorSonnek extends \System
                 }
             }
             asort($arrNewFiles);
-            foreach ($arrNewFiles as $newFileSRC)
-            {
+            foreach ($arrNewFiles as $newFileSRC) {
                 // Add new entry in the dbafs
                 \Dbafs::addResource($newFileSRC);
                 // Write new entry to tl_gallery_creator_pictures
@@ -157,8 +139,7 @@ class GalleryCreatorSonnek extends \System
         //die(print_r($objModule->Template,true));
         global $objPage;
         $objPage->pageTitle = 'Bildergalerie';
-        if ($objAlbum !== null)
-        {
+        if ($objAlbum !== null) {
             // display the album name in the head section of your page (title tag)
             $objPage->pageTitle = specialchars($objAlbum->name);
             // display the album comment in the head section of your page (description tag)
